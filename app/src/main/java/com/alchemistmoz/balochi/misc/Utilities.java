ea4e7c1cfe05e91572afa9231b31a52da44215a1
MortazaG <mortaza30@gmail.com>
2019-06-07 22:29:01 +0200

    Get activity from context.

--- app/src/main/java/com/alchemistmoz/balochi/misc/Utilities.java
+++ app/src/main/java/com/alchemistmoz/balochi/misc/Utilities.java
@@ -1,6 +1,8 @@
 package com.alchemistmoz.balochi.misc;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -242,5 +244,15 @@ public class Utilities {
             recyclerView.setLayoutAnimation(animation);
             recyclerView.setLayoutAnimationListener(animationListener);
         }
+    }
+
+    /**
+     * Get activity instance from desired context.
+     */
+    public static Activity getActivity(Context context) {
+        if (context == null) return null;
+        if (context instanceof Activity) return (Activity) context;
+        if (context instanceof ContextWrapper) return getActivity(((ContextWrapper)context).getBaseContext());
+        return null;
     }
 }
