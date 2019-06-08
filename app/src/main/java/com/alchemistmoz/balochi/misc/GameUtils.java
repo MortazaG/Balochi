f6929efad31ac3624eea1fd99c6927e0a429b491
MortazaG <mortaza30@gmail.com>
2019-06-08 22:46:47 +0200

    Add Utilities for Games

---
+++ app/src/main/java/com/alchemistmoz/balochi/misc/GameUtils.java
@@ -0,0 +1,16 @@
+package com.alchemistmoz.balochi.misc;
+
+/**
+ * Utilities class with various useful methods to be used
+ * across different games.
+ *
+ */
+public final class GameUtils {
+
+    /**
+     * Prevent user from instantiating the class.
+     */
+    private GameUtils() {
+        // Empty
+    }
+}
