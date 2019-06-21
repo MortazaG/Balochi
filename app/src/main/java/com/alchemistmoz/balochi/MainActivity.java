fd12d56d5f71db7699a3039a9759fa1af47825b2
MortazaG <mortaza30@gmail.com>
2019-06-21 07:26:37 +0200

    Remove library

--- app/src/main/java/com/alchemistmoz/balochi/MainActivity.java
+++ app/src/main/java/com/alchemistmoz/balochi/MainActivity.java
@@ -1,6 +1,5 @@
 package com.alchemistmoz.balochi;
 
-import android.media.FaceDetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
