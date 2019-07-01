bd3dc27c456db424101f309f9fa7391b82c0ca61
MortazaG <mortaza30@gmail.com>
2019-07-01 22:04:43 +0200

    Update menu images

--- app/src/main/java/com/alchemistmoz/balochi/categories/face/FaceActivity.java
+++ app/src/main/java/com/alchemistmoz/balochi/categories/face/FaceActivity.java
@@ -51,8 +51,8 @@ public class FaceActivity extends AppCompatActivity {
 
         // Add new Category objects to the ArrayList
         categories.add(new Category(R.drawable.face_item4, R.raw.menu_repetition, RepetitionActivity.class));
-        categories.add(new Category(R.drawable.colors_menu_memory, R.raw.menu_repetition, MemoryActivity.class));
-        categories.add(new Category(R.drawable.colors_menu_audiomatch5, R.raw.menu_repetition, AudioMatchActivity.class));
+        categories.add(new Category(R.drawable.face_menu_memory, R.raw.menu_memory, MemoryActivity.class));
+        categories.add(new Category(R.drawable.face_menu_audiomatch, R.raw.menu_repetition, AudioMatchActivity.class));
 
         // Setup the recycler view
         recyclerView = Utilities.initLinearRecyclerView(this, R.id.recycler_view_list);
