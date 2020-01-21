4a3da128f9cc90ac4118712bbf935c5251a16de6
MortazaG <mortaza30@gmail.com>
2020-01-21 12:16:10 +0100

    Rearrange menu items order

--- app/src/main/java/com/alchemistmoz/balochi/MainActivity.java
+++ app/src/main/java/com/alchemistmoz/balochi/MainActivity.java
@@ -56,9 +56,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Add new Category objects to the ArrayList
         categories.add(new Category(R.drawable.numbers5, R.raw.menu_numbers, NumbersActivity.class));
+        categories.add(new Category(R.drawable.colors3, R.raw.menu_colors, ColorsActivity.class));
         categories.add(new Category(R.drawable.face_item, R.raw.menu_repetition, FaceActivity.class));
         categories.add(new Category(R.drawable.family1, R.raw.menu_family, FamilyActivity.class));
-        categories.add(new Category(R.drawable.colors3, R.raw.menu_colors, ColorsActivity.class));
         categories.add(new Category(R.drawable.fruitveggies1, R.raw.menu_fruitveggies, FruitVeggiesActivity.class));
 
         // Setup the recycler view with the id of the recycler view to be used and type of layout
